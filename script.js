1619e0aabf86889e62069c21da75443b9e43aa1b
Shelesh Badola <badolashelesh@gmail.com>
2020-12-18 15:49:47 +0530

    projected completed

--- script.js
+++ script.js
@@ -6,14 +6,14 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Shelesh Badola',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Jasmine Dhillon',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -73,16 +73,21 @@ const currencies = new Map([
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /***********************Movements Elements*****************************************/
-function movement(account){
-containerMovements.innerHTML='';
-let values =account.movements.map(x =>{
-let type = x>0?'deposit':'withdrawal';
-return  containerMovements.insertAdjacentHTML('afterbegin',`<div class="movements__row">
-<div class="movements__type movements__type--${type}">${type}</div>
-<div class="movements__value">${x}₹</div>
-</div>`);
-});
-}
+
+function movement(movements,sort=false){
+  containerMovements.innerHTML='';
+  const mov = sort?movements.slice().sort((a,b)=>
+  {return a-b}):movements;
+  let values =mov.map((x,i) =>{
+  let type = x>0?'deposit':'withdrawal';
+  return  containerMovements.insertAdjacentHTML('afterbegin',`<div class="movements__row">
+  <div class="movements__type movements__type--${type}">${i+1} ${type}</div>
+  <div class="movements__value">${x}₹</div>
+  </div>`);
+  });
+  }
+
+
 /**************************************In and Out Values**************************************************/
 
 function InandOut(account){
@@ -106,10 +111,10 @@ let totalNegative =negativeArray.reduce(function(a,b){
 },0);
 labelSumOut.innerHTML=`${totalNegative}₹`;
 //Total Sum
-let TotalAmount=account.movements.reduce(function(a,b){
+account.balance=account.movements.reduce(function(a,b){
   return a+b;
 },0);
-labelBalance.innerHTML=`${TotalAmount}₹`;
+labelBalance.innerHTML=`${account.balance}₹`;
 //Interest Values
 const interest =account.movements
                 .filter(x=>x>0)
@@ -150,10 +155,82 @@ btnLogin.addEventListener('click',function(e){
     labelWelcome.textContent=`Welcome ${currentAccount.owner.split(' ')[0]}`
     /***Shows app after login */
     containerApp.style.opacity=100;
-    //****In and outs function */
-    InandOut(currentAccount);
-    /******Movements*/
-    movement(currentAccount);
+    //update UI
+    updateUI(currentAccount);
+   
    }
+   console.log(currentAccount);
 });
+/****Update UI */
+function updateUI(acc){
+   //****In and outs function */
+   InandOut(acc);
+   /******Movements*/
+   movement(acc.movements);
+}
+
+
+
+/*****************Transfer Money from 1 account to another*/
+btnTransfer.addEventListener('click',function(account){
+  account.preventDefault();
+const amountTransfer=Number(inputTransferAmount.value);
+const receiverAccount=accounts.find(acc=>
+  acc.username===inputTransferTo.value); 
+  console.log(amountTransfer,receiverAccount);
+  if(amountTransfer>0 && currentAccount.balance>=amountTransfer &&
+    receiverAccount &&
+     receiverAccount?.username !==currentAccount.username){
+       //Doing the transfer
+    currentAccount.movements.push(-amountTransfer);
+    receiverAccount.movements.push(amountTransfer);
+    updateUI(currentAccount);
+  }
+  inputTransferAmount.blur();
+  inputTransferTo.blur();
+});
+console.log(currentAccount);
+/************Request Loan feature ***********/
+btnLoan.addEventListener('click',function(e){
+    e.preventDefault();
+    const amount =Number(inputLoanAmount.value);
+if(amount>0 && currentAccount.movements.some(mov =>
+  mov>= amount*0.1)){
+ currentAccount.movements.push(amount);
+ updateUI(currentAccount);
+  }
+  inputLoanAmount.value='';
+});
+/**********************Deleting an Account******************/
+btnClose.addEventListener('click',function(e){
+  e.preventDefault();
+//confirming the credentials
+  if(inputCloseUsername.value===currentAccount.username 
+  && Number(inputClosePin.value)===currentAccount.pin ){
+//Deleting the user from accounts
+    let index =accounts.findIndex(acc=>
+    acc.username===currentAccount.username);
+  accounts.splice(index,1);
+  //removing the ui
+  containerApp.style.opacity=0;
+  }
+});
+/********Updating Dates */
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+labelDate.textContent=`${today}`;
+/*****Button Sort */
+let sorted =false;
+btnSort.addEventListener('click',function(e){
+  e.preventDefault();
+  movement(currentAccount.movements,!sorted);
+  sorted=!sorted;
+  console.log("working");
+});
+
 /////////////////////////////////////////////////
